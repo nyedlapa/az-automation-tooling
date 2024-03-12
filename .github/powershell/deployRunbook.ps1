25bedc644e98bb67c4aa33bbb73ba67b89dbd95a
nareshyedlapalli <160072851+nareshyedlapalli@users.noreply.github.com>
2024-03-12 20:43:14 +0530

    Update deployRunbook.ps1

--- .github/powershell/deployRunbook.ps1
+++ .github/powershell/deployRunbook.ps1
@@ -3,4 +3,4 @@ param($ResourceGroup, $automationAccountName)
 $clientSecretExpiration = 'clientSecretExpiration'
 
 
-Import-AzAutomationRunbook -AutomationAccountName $automationAccountName -ResourceGroupName $ResourceGroup -Name $clientSecretExpiration -Path "./infrastructure/runbooks/client-secret-expiration.ps1" -Published -Type PowerShell -Force
+Import-AzAutomationRunbook -AutomationAccountName $automationAccountName -ResourceGroupName $ResourceGroup -Name $clientSecretExpiration -Path "./infrastructure/runbooks/client-secret-expiration.ps1" -Published -Type PowerShell -Force -Version '7.0'
