94da1a7f86f350aa1fd6f7a10264351fc5d13077
nareshyedlapalli <160072851+nareshyedlapalli@users.noreply.github.com>
2024-03-13 20:29:55 +0530

    Update deployRunbook.ps1

--- .github/powershell/deployRunbook.ps1
+++ .github/powershell/deployRunbook.ps1
@@ -3,4 +3,12 @@ param($ResourceGroup, $automationAccountName)
 $clientSecretExpiration = 'clientSecretExpiration'
 
 
-Import-AzAutomationRunbook -AutomationAccountName $automationAccountName -ResourceGroupName $ResourceGroup -Name $clientSecretExpiration -Path "./infrastructure/runbooks/client-secret-expiration.ps1" -Published -Type PowerShell -Force -Version '7.2'
+Import-AzAutomationRunbook -AutomationAccountName $automationAccountName -ResourceGroupName $ResourceGroup -Name $clientSecretExpiration -Path "./infrastructure/runbooks/client-secret-expiration.ps1" -Published -Type PowerShell -Force
+
+# Get the imported runbook
+$runbook = Get-AzAutomationRunbook -AutomationAccountName $automationAccountName -ResourceGroupName $ResourceGroup -Name $clientSecretExpiration
+
+# Update the runbook to use PowerShell version 7.2
+$runbook.Properties.RunbookType = 'PowerShell72'
+Update-AzAutomationRunbook -AutomationAccountName $automationAccountName -ResourceGroupName $ResourceGroup -Name $clientSecretExpiration -Runbook $runbook.Properties
+
